01f18d13dec03a5cbd631c300fbad9b061ac25f2
Johan Karlsson <93186588+gurkvatten@users.noreply.github.com>
2025-10-22 13:51:05 +0200

    Shipping interface

--- src/main/java/com/example/Shippable.java
+++ src/main/java/com/example/Shippable.java
@@ -1,4 +1,11 @@
 package com.example;
 
-public class Shippable {
+import java.math.BigDecimal;
+
+public interface Shippable {
+
+    BigDecimal calculateShippingCost();
+
+    double weight();
+
 }
