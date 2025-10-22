d4a62d71b01d6afc92a9402f1b02638c41cb55c2
Johan Karlsson <93186588+gurkvatten@users.noreply.github.com>
2025-10-22 13:49:50 +0200

    Perishable interface

--- src/main/java/com/example/Perishable.java
+++ src/main/java/com/example/Perishable.java
@@ -1,4 +1,11 @@
 package com.example;
+import java.time.LocalDate;
 
-public class Perishable {
+public interface Perishable {
+
+    LocalDate expirationDate();
+
+    default boolean isExpired() {
+        return expirationDate().isBefore(LocalDate.now());
+    }
 }
